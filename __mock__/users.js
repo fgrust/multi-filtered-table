9187703a159a30a0164ec9a5a1d00c0b202a7f39
mben-code <michael.benares2020@gmail.com>
2021-06-21 08:10:33 -0400

    create index page

---
+++ __mock__/users.js
@@ -0,0 +1,97 @@
+export const users = [
+  {
+    id: '34FDFA324FE',
+    name: 'Mary Paradis',
+    address: '3679 Junior Avenue',
+    city: 'Levelland',
+    state: 'TX',
+    zip: '79336',
+  },
+  {
+    id: 'E3256GW234F',
+    name: 'Alisha Bryant',
+    address: '2748 Oliver Street',
+    city: 'Weatherford',
+    state: 'TX',
+    zip: '76086',
+  },
+  {
+    id: 'HW3234FD3452',
+    name: 'Michael Markley',
+    address: '3480 Shadowmar Drive',
+    city: 'Metairie',
+    state: 'LA',
+    zip: '70001',
+  },
+  {
+    id: 'P3234623EF43',
+    name: 'Amanda Velez',
+    address: '3217 Farland Street',
+    city: 'Montandon',
+    state: 'PA',
+    zip: '17850',
+  },
+  {
+    id: 'C234E4234223',
+    name: 'Darlene Epps',
+    address: '173 Poplar Lane',
+    city: 'Thompsonville',
+    state: 'MI',
+    zip: '49683',
+  },
+  {
+    id: 'W3423FFA435D',
+    name: 'Mark Madrid',
+    address: '1396 Lucky Duck Drive',
+    city: 'Centerburg',
+    state: 'OH',
+    zip: '43011',
+  },
+  {
+    id: '4534FASD23423',
+    name: 'Jason',
+    address: '289 Pinnickinick Street',
+    city: 'La Canada Flintridge',
+    state: 'CA',
+    zip: '91012',
+  },
+  {
+    id: 'KE34334C34212',
+    name: 'Jack Medina',
+    address: '81 Randolph Street',
+    city: 'Pawtucket',
+    state: 'MA',
+    zip: '02860',
+  },
+  {
+    id: 'M34234E7865C',
+    name: 'Margie Brown',
+    address: '3601 Main Street',
+    city: 'Kirkland',
+    state: 'WA',
+    zip: '98034',
+  },
+]
+
+export const headers = [
+  {
+    name: 'name',
+    display: 'Name',
+  },
+  {
+    name: 'address',
+    display: 'Address',
+  },
+  {
+    name: 'city',
+    display: 'City',
+  },
+  {
+    name: 'state',
+    display: 'State',
+  },
+  {
+    name: 'zip',
+    display: 'Zip',
+  },
+]

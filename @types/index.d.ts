eaf6520725f3c0a4b22aa0de12755f9f201962ec
mben-code <michael.benares2020@gmail.com>
2021-06-21 08:07:30 -0400

    add types

---
+++ @types/index.d.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  name: string
+  address: string
+  city: string
+  state: string
+  zip: string
+}
+
+export interface ITableHeader {
+  name: string
+  display?: string
+}
